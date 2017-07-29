70e26f5044ba61d073d563819b37643fc5f419b3
TarunSharma <Tarun-sharma_xft@external.mckinsey.com>
2017-07-30 00:17:38 +0530

    moved to a base folder

---
+++ array and String/questions-02.js
@@ -0,0 +1,12 @@
+
+
+What is the event loop? Can you draw a simple diagram to explain event loop?
+How to you explain closure?
+How would you make sure value of this works correctly inside setTimeout?
+What are the different possible values for this?
+What is debounce and how could you implement debounce?
+How would you communicate with server
+Explain Promise to your grandmother
+If and website is slow how what would you do to make it faster?
+What ES6 feature do you use other than let, var and arrow?
+What build tool you use and tell me some advantages to use that build toolquestions-02.js
---
+++ questions-02.js
@@ -0,0 +1,10 @@
+
+
+What is the difference between apply, call, and bind?
+What is event delegation?
+What is event bubbling?
+What is hoisting and how does it work?
+What is the prototype chain?
+What determines the value of ‘this’?
+What is the event loop?
+Implement curry.
